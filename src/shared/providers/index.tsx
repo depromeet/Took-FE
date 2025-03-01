7492fe4e05c63a3665ec5aa0ab21fa6592e17a96
semnil5202 <semnil5202@gmail.com>
2025-03-01 14:46:59 +0900

    ♻️ refactor: server state 고려

--- src/shared/providers/index.tsx
+++ src/shared/providers/index.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient();
+import { QueryClientProvider } from './QueryClientProvider';
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider>
       {children}
       <ReactQueryDevtools />
     </QueryClientProvider>
