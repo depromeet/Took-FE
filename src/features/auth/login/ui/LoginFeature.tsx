ea3daecdd95681291814117039bd18f11e0d5291
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-01 19:01:34 +0900

    ♻️refactor : 중복 css 코드 제거

--- src/features/auth/login/ui/LoginFeature.tsx
+++ src/features/auth/login/ui/LoginFeature.tsx
@@ -5,7 +5,7 @@ import KakaoLoginButton from '../../ui/KakaoLoginButton';
 export function LoginFeature() {
   const imgUrl = '/images/login/onboarding/onboarding-login.png';
   return (
-    <div className="flex h-full w-full max-w-[600px] flex-col">
+    <div className="flex h-full w-full flex-col">
       <div className="relative flex h-full w-full flex-col items-center justify-between">
         {/* 이미지 배치가 안되서 임시로 top-[45%]사용했습니다. */}
         <div className="absolute left-1/2 top-[45%] h-[23%] w-full -translate-x-1/2 -translate-y-1/2 transform">
--- src/features/onboarding/ui/OnboardingFeature.tsx
+++ src/features/onboarding/ui/OnboardingFeature.tsx
@@ -3,6 +3,14 @@
 import OnboardingCarousel from './OnboardingCarousel';
 import { OnboardingHeader } from './OnboardingHeader';
 
+/**
+ * OnboardingFeatureProps 설명
+ * onComplete , onSkip은 showOnboarding 상태를 false로 바구는 함수입니다.
+ * 헤더의 건너뛰기 , 모든 온보딩 페이지를 다 확인했을 때 사용합니다.
+ * onSkip = 건너뛰기 누를 때 사용
+ * onComplete = 온보딩 마지막 페이지에서 사용
+ */
+
 interface OnboardingFeatureProps {
   onComplete: () => void;
   onSkip: () => void;
@@ -10,7 +18,7 @@ interface OnboardingFeatureProps {
 
 export function OnboardingFeature({ onComplete, onSkip }: OnboardingFeatureProps) {
   return (
-    <div className="flex h-full w-full max-w-[600px] flex-col">
+    <div className="flex h-full w-full flex-col">
       <OnboardingHeader onSkip={onSkip} />
       <OnboardingCarousel onComplete={onComplete} />
     </div>
