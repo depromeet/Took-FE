1d7788634afa921aa352f98e03a6a50398717d95
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-02 18:02:37 +0900

    ✨ feat :  공통 컴포넌트(progressBar) 구현 완료

---
+++ src/shared/ui/progressBar/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { cn } from '@/shared/lib/utils';
+import { Progress } from '@/shared/ui/progressBar/progress';
+
+interface ProgressBarProps {
+  currentStep: number;
+  totalSteps: number;
+  color?: string;
+  className?: string;
+}
+
+export const ProgressBar = ({ currentStep, totalSteps, color, className }: ProgressBarProps) => {
+  // 진행률 계산 (0-100 사이의 값)
+  const progressPercentage = Math.min(Math.max(Math.round((currentStep / totalSteps) * 100), 0), 100);
+
+  return (
+    <div className="w-full space-y-2">
+      <Progress value={progressPercentage} color={color} className={cn(className)} />
+    </div>
+  );
+};
+
+export default ProgressBar;
