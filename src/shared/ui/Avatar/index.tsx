3f4665a719f4963c2c03e4a67097c4c9b8d3641b
youngju6143 <youngju6143@naver.com>
2025-03-03 18:59:17 +0900

    📝 Docs: 공통 컴포넌트 Avatar 사용 방법 주석 수정

--- src/shared/ui/Avatar/index.tsx
+++ src/shared/ui/Avatar/index.tsx
@@ -1,3 +1,14 @@
+/**
+ * 공통 컴포넌트 - Avatar
+ *
+ * 사용 방법:
+ * <WrappedAvatar src={이미지 URL} alt="이미지 설명" size="large" />
+ *
+ * @param {string} [src] - 표시할 이미지의 URL. 없을 경우 기본 아이콘이 표시됩니다.
+ * @param {string} [alt] - 이미지 설명 텍스트. 이미지 로드 실패 시 표시됩니다.
+ * @param {'large' | 'medium' | 'small'} [size='large'] - 아바타 크기. 기본값은 'large'입니다.
+ */
+
 import React from 'react';
 
 import AvatarIcon from '../icon/avatarIcon.svg';
