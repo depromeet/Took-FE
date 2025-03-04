37e15d413949a456d3641214688511036b4d702a
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-04 23:08:26 +0900

    ♻️refactor : tag 컴포넌트 props 수정

--- src/shared/ui/tag/tag.tsx
+++ src/shared/ui/tag/tag.tsx
@@ -8,47 +8,36 @@ import { cn } from '@/shared/lib/utils';
 
 import { Typography } from '../typography';
 
-/** 공통 컴포넌트 - tag
- * Tag 컴포넌트는 다양한 크기와 옵션을 가진 태그를 렌더링합니다.
- *
- * @param {object} props - 컴포넌트 속성 객체
- * @param {string} props.message - 태그에 표시할 텍스트
- * @param {"sm" | "md" | "lg"} [props.size="md"] - 태그의 크기를 설정합니다.
- * @param {boolean} [props.hasCloseButton=false] - 닫기 버튼 표시 여부를 결정합니다.
- * @param {string} [props.backgroundColor="bg-gray-600"] - 배경 색상을 설정합니다.
- * @param {Function} [props.onClose] - 닫기 버튼 클릭 시 실행할 함수입니다.
- * @param {string} [props.className] - 추가적인 클래스명입니다.
- * @returns {JSX.Element} 렌더링된 Tag 컴포넌트
- *
- * @example
- * // 기본 사용법
- * <Tag message="기본 태그" />
- *
- * // 다양한 크기와 옵션 사용
- * <Tag message="작은 태그" size="sm" hasCloseButton={true} backgroundColor="bg-primary-hover" />
- * <Tag message="중간 태그" size="md" hasCloseButton={false} />
- * <Tag message="큰 태그" size="lg" hasCloseButton={true} />
- */
-
 type TagSize = 'sm' | 'md' | 'lg';
 
 interface TagProps {
   message: string;
-  backgroundColor?: string;
-  hasCloseButton?: boolean;
   size?: TagSize;
   onClose?: () => void;
   className?: string;
 }
 
-function Tag({
-  message,
-  backgroundColor = 'bg-gray-600',
-  hasCloseButton = false,
-  size = 'md',
-  onClose,
-  className = '',
-}: TagProps) {
+/** 공통 컴포넌트 - tag
+ * @description Tag 컴포넌트는 다양한 크기와 옵션을 가진 태그를 렌더링합니다.
+ *
+ * @param {object} props - 컴포넌트 속성 객체
+ * @param {string} props.message - 태그에 표시할 텍스트
+ * @param {"sm" | "md" | "lg"} [props.size="md"] - 태그의 크기를 설정합니다.
+ * @param {Function} [props.onClose] - 닫기 버튼 클릭 시 실행할 함수입니다. 제공된 경우에만 닫기 버튼이 표시됩니다.
+ * @param {string} [props.className="bg-gray-300"] - 추가적인 클래스명입니다. 배경색을 포함한 스타일 지정에 사용됩니다.
+ * @returns {JSX.Element} 렌더링된 Tag 컴포넌트
+ *
+ * @example
+ * // 기본 사용법
+ * <Tag message="기본 태그" /> -기본 색상 bg-gray-300
+ *
+ * // 다양한 크기와 옵션 사용
+ * <Tag message="작은 태그" size="sm" className="bg-primary-light-active" />
+ * <Tag message="중간 태그" size="md" />
+ * <Tag message="큰 태그" size="lg" onClose={handleCloseBtn} />
+ */
+
+function Tag({ message, size = 'md', onClose, className = 'bg-gray-300' }: TagProps) {
   const tagStyles = cva('inline-flex items-center rounded-full', {
     variants: {
       size: {
@@ -70,9 +59,9 @@ function Tag({
   };
 
   return (
-    <span className={cn(tagStyles({ size }), backgroundColor, className)}>
+    <span className={cn(tagStyles({ size }), className)}>
       <Typography variant={size === 'lg' ? 'body-2' : size === 'md' ? 'body-3' : 'caption-1'}>{message}</Typography>
-      {hasCloseButton && (
+      {onClose && (
         <button
           type="button"
           onClick={handleClose}
