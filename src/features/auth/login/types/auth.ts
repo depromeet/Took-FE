a89558787d00905700c0e902d8da936871c01166
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-07 13:10:15 +0900

    ✨ feat : 로그인 API  타입 지정

---
+++ src/features/auth/login/types/auth.ts
@@ -0,0 +1,35 @@
+// POST - api/auth/login/{oauthType}
+
+export type ApiResponse<T> = {
+  status: string;
+  message: string;
+  timestamp: string;
+  data: T;
+};
+
+export type UserDto = {
+  id: number;
+  name: string;
+};
+
+export type TokenDto = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type AuthResponseDto = {
+  token: TokenDto;
+  user: UserDto;
+};
+
+export type AuthDto = ApiResponse<AuthResponseDto>;
+
+// GET - api/auth/{oauthType}
+export type RedirectDto = {
+  status: string;
+  message: string;
+  timestamp: string;
+  data: {
+    url: string;
+  };
+};
