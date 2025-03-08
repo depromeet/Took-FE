0ebf4cf0771758f2a7126e66f3df8dcd83f1f1a2
revibio-semnil5202 <sm.lee@fanmaum.com>
2025-03-08 17:07:35 +0900

    refactor: maximum scale 1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { pretendard } from '@/shared/lib/font';
 import { Providers } from '@/shared/providers';
 
@@ -17,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
       <body className={`${pretendard.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
