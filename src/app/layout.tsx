f640716809fe530569d92625b67e814087a71978
revibio-semnil5202 <sm.lee@fanmaum.com>
2025-03-08 17:17:01 +0900

    feat: viewport override

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
-import Head from 'next/head';
-
 import { pretendard } from '@/shared/lib/font';
 import { Providers } from '@/shared/providers';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Took',
   description: 'Took name service',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -19,9 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </Head>
       <body className={`${pretendard.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
