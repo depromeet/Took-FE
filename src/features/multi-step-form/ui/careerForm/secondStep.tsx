eccf18c0c42388205383454ee6925aed1718b7f9
youngju6143 <youngju6143@naver.com>
2025-03-09 02:38:39 +0900

    ✏️ Fix: fix husky error

--- src/features/multi-step-form/ui/careerForm/secondStep.tsx
+++ src/features/multi-step-form/ui/careerForm/secondStep.tsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react';
 
 import Ball from '@/features/multi-step-form/ui/careerForm/tagFormStep/ui/Ball';
 import TagBox from '@/features/multi-step-form/ui/careerForm/tagFormStep/ui/TagBox';
-import Header from '@/shared/ui/header';
 import { Button } from '@/shared/ui/button';
+import Header from '@/shared/ui/header';
 
 function SecondStep() {
   const [tagCount, setTagCount] = useState(0);
