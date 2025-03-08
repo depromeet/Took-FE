2fabf3c954a66849619af6a38c46633b45cf2045
jangwonyoon <wkddnjs7310@naver.com>
2025-03-08 16:40:37 +0900

    ✨ Feat: 카드 생성 multi-step-form 1차 구현

--- src/features/multi-step-form/ui/careerForm/firstStep.tsx
+++ src/features/multi-step-form/ui/careerForm/firstStep.tsx
@@ -1,11 +1,22 @@
+import { Controller, useFormContext } from 'react-hook-form';
+
 import { cn } from '@/shared/lib/utils';
 import { spacingStyles } from '@/shared/spacing';
 import WrappedInput from '@/shared/ui/Input';
+import WrappedTagInput from '@/shared/ui/Input/tagInput';
 
 import AvatarImg from '../../components/AvartarImg';
 import { CAREER_FORM } from '../../config';
+import { CareerFormData } from '../../schema';
+
+import ErrorMsg from './errorMsg';
 
 function FirstStep() {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<CareerFormData>();
+
   return (
     <>
       <header className='flex flex-col gap-3'>
@@ -17,10 +28,47 @@ function FirstStep() {
           <div className='flex justify-center'>
             <AvatarImg />
           </div>
-          <WrappedInput title='이름' placeholder='이름을 입력해주세요.' />
-          <WrappedInput title='세부직군' placeholder='세부직군' />
-          <WrappedInput title='관심 도메인' placeholder='관심도메인' />
-          <WrappedInput title='한 줄 소개' placeholder='본인을 잘 드러낼 수 있는 문장을 작성해 주세요.' />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <>
+                <WrappedInput title='이름' placeholder='이름을 입력해주세요.' {...field} />
+                {errors.name && <ErrorMsg errorMsg={errors.name.message} />}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="detail_career"
+            render={({ field }) => (
+              <>
+                <WrappedInput title='세부직군' placeholder='세부직군' {...field} />
+                {errors.detail_career && <ErrorMsg errorMsg={errors.detail_career.message} />}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="domain"
+            render={({ field }) => (
+              <>
+                <WrappedTagInput title='관심 도메인' placeholder='관심 도메인' {...field} />
+                {/* <WrappedInput title='관심 도메인' placeholder='관심도메인' {...field} />
+                {errors.domain && <ErrorMsg errorMsg={errors.domain.message} />} */}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="description"
+            render={({ field }) => (
+              <>
+                <WrappedInput title='한 줄 소개' placeholder='본인을 잘 드러낼 수 있는 문장을 작성해 주세요.' {...field} />
+                {errors.description && <ErrorMsg errorMsg={errors.description.message} />}
+              </>
+            )}
+          />
         </div>
       </section>
     </>
