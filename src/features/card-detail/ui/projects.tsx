ec7648a1a3d4cdd0df32b379d3cc2d411c7d3f83
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-11 19:03:36 +0900

    ✨feat : 명함 상세 페이지 대표 프로잭트 컴포넌트 구현

--- src/features/card-detail/ui/projects.tsx
+++ src/features/card-detail/ui/projects.tsx
@@ -3,24 +3,20 @@ import React from 'react';
 
 import { spacingStyles } from '@/shared/spacing';
 
-import { ProjectDto } from '../types/sample';
+import { ProjectDto } from '../types/cardDetail';
 
 interface ProjectsProps {
   data: ProjectDto[];
 }
 
 function Projects({ data }: ProjectsProps) {
+  console.log('ProjectsProps  :' + data[0].imageUrl);
   return (
     <div className={`grid grid-cols-2 gap-4 ${spacingStyles({ marginTop: 'xl' })}`}>
-      {data.map((project) => (
-        <div key={project.id} className="flex flex-col overflow-hidden rounded-md">
+      {data.map((project, i) => (
+        <div key={i} className="flex flex-col overflow-hidden rounded-md">
           <div className="relative h-40 w-full border-[1px] border-opacity-white-20">
-            <Image
-              src={project.thumbnail || '/icons/imageIcon.svg'}
-              alt={project.title}
-              fill
-              className="object-cover"
-            />
+            <Image src={project.imageUrl || '/icons/imageIcon.svg'} alt={project.title} fill className="object-cover" />
           </div>
 
           <div className={`flex flex-col ${spacingStyles({ marginTop: 'sm' })}`}>
