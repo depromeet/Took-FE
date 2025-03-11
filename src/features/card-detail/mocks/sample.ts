d2bd3bcb8a4553bc16ac75404e4c967652ec8058
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-11 21:31:09 +0900

    ♻️ refactor: 명함 상세 임시 데이터 수정

--- src/features/card-detail/mocks/sample.ts
+++ src/features/card-detail/mocks/sample.ts
@@ -42,13 +42,13 @@ export const mockCardDetailData: CardDetailDto = {
       {
         title: 'Spring Boot로 RESTful API 만들기',
         link: 'https://blog.example.com/posts/123',
-        imageUrl: '',
+        imageUrl: '/icons/imageIcon.svg',
         description: 'Spring Boot를 이용한 RESTful API 개발 방법을 소개합니다.',
       },
       {
         title: 'Spring Boot로 RESTful API 만들기',
         link: 'https://blog.example.com/posts/123',
-        imageUrl: '',
+        imageUrl: '/icons/imageIcon.svg',
         description: 'Spring Boot를 이용한 RESTful API 개발 방법을 소개합니다.',
       },
     ],
@@ -56,13 +56,13 @@ export const mockCardDetailData: CardDetailDto = {
       {
         title: 'UI 컴포넌트 라이브러리',
         link: 'https://github.com/username/project',
-        imageUrl: '',
+        imageUrl: '/icons/imageIcon.svg',
         description: 'React와 TypeScript로 만든 UI 컴포넌트 모음',
       },
       {
         title: '개발자 네트워킹 플랫폼',
         link: 'https://github.com/username/project',
-        imageUrl: '',
+        imageUrl: '/icons/imageIcon.svg',
         description: '개발자들이 서로 연결되고 프로젝트를 공유하는 앱',
       },
     ],
