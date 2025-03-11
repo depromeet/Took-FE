01e352719bb6699d32403d84e2174feea975f58f
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-11 19:03:11 +0900

    ✨feat : 명함 상세 페이지 도메인 컴포넌트 구현

--- src/features/card-detail/ui/domain.tsx
+++ src/features/card-detail/ui/domain.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 import { spacingStyles } from '@/shared/spacing';
 import Tag from '@/shared/ui/tag/tag';
 
-import { DomainDto } from '../types/sample';
-
 interface DomainListProps {
-  data: DomainDto[];
+  data: string[];
 }
 
 function DomainList({ data }: DomainListProps) {
   return (
     <div className={`flex flex-wrap gap-2 ${spacingStyles({ marginTop: 'ms' })}`}>
-      {data.map((e, i) => {
-        return <Tag key={i} message={e.name} className="bg-opacity-purple-30" />;
+      {data.map((name, i) => {
+        return <Tag key={i} message={name} className="line-clamp-1 bg-opacity-purple-30" />;
       })}
     </div>
   );
