d5727294d61178f6677b7b82bc7b89f2faa925fa
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-13 00:41:47 +0900

    ✨ feat : getToken(토큰 저장 함수 ) 구현 완료

--- src/features/auth/login/apis/getToken.ts
+++ src/features/auth/login/apis/getToken.ts
@@ -9,10 +9,11 @@ import { AuthDto, SocialProvider } from '../types/auth';
 
 export async function getToken(provider: SocialProvider, code: string): Promise<AuthDto> {
   try {
-    const response = await client.post<null, AuthDto>(
+    const data = await client.post<null, AuthDto>(
       `${CLIENT_SIDE_URL}/api/auth/login/${provider.toUpperCase()}?code=${code}`,
     );
-    return response;
+    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
     throw error;
@@ -23,8 +24,8 @@ export async function handleSocialAuth(provider: SocialProvider, code: string) {
   try {
     const tokenData: AuthDto = await getToken(provider, code);
 
-    setCookie('accessToken', tokenData.data.token.accessToken);
-    setCookie('refreshToken', tokenData.data.token.refreshToken);
+    setCookie('accessToken', tokenData.data.data.token.accessToken);
+    setCookie('refreshToken', tokenData.data.data.token.refreshToken);
     return { success: true };
   } catch (error) {
     console.error(`${provider} 인증 처리 실패:`, error);
