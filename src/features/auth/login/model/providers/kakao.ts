5f2dbc90638064ef4cafc565d8409973ed8c7c7e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-13 00:42:44 +0900

    ✨ feat : 로컬 카카오 리다이렉트 경로 수정

--- src/features/auth/login/model/providers/kakao.ts
+++ src/features/auth/login/model/providers/kakao.ts
@@ -4,7 +4,7 @@ export const PROVIDER_ID = 'KAKAO';
 
 export const kakaoLogin = () => {
   const restApiKey = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI_LOCAL;
 
   if (!restApiKey || !redirectUri) {
     console.error('카카오 로그인 설정이 잘못되었습니다.');
