17066f35e05963013c413c7f8c59d33b780697e2
semnil5202 <semnil5202@gmail.com>
2025-03-15 15:54:09 +0900

    design: add home background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import 'swiper/css/pagination';
 
 import { Toaster } from 'sonner';
 
+import { HomeBackground } from '@/features/home/components/BusinessCard/Background/HomeBackground';
 import { CardContainer } from '@/features/home/containers/CardContainer';
 import { ClipboardContainer } from '@/features/home/containers/ClipboardContainer';
 import { HeaderContainer } from '@/features/home/containers/HeaderContainer';
@@ -10,6 +11,7 @@ import { HeaderContainer } from '@/features/home/containers/HeaderContainer';
 export default function Home() {
   return (
     <div className="relative mx-auto h-dvh w-full max-w-[600px] justify-center">
+      <HomeBackground className="absolute" />
       <HeaderContainer />
       <section className="absolute left-1/2 top-1/2 h-auto w-full -translate-x-1/2 -translate-y-1/2 pb-20">
         <CardContainer />
