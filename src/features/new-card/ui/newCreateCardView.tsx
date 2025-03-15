49b51b1e887c5d9d9b81acb88ad724818f5659ae
jangwonyoon <wkddnjs7310@naver.com>
2025-03-15 16:14:16 +0900

    🩺 Style: 카드생성 온보딩 style 수정

--- src/features/new-card/ui/newCreateCardView.tsx
+++ src/features/new-card/ui/newCreateCardView.tsx
@@ -12,34 +12,26 @@ function NewCreateCardView() {
 
   const handleCreateDesignCard = () => {
     router.push(ROUTE_PATH.design);
-  }
+  };
 
   const handleCreateDevCard = () => {
     router.push(ROUTE_PATH.dev);
-  }
+  };
 
   return (
     <main className={cn('flex flex-col', spacingStyles({ paddingX: 'ml', paddingY: 'lg' }))}>
-      <header className='flex flex-col gap-3'>
-        <h1 className='text-title-1 text-gray-white'>{CAREER_SELECT.title}</h1>
-        <p className='text-body-3 text-gray-400'>{CAREER_SELECT.description}</p>
+      <header className="flex flex-col gap-3">
+        <h1 className="text-title-1 text-gray-white">{CAREER_SELECT.title}</h1>
+        <p className="text-body-3 text-gray-400">{CAREER_SELECT.description}</p>
       </header>
       <section className={cn(spacingStyles({ marginTop: 'xl' }))}>
         <List>
-          <WrappedListItem
-            leftIcon='design'
-            text={CAREER_SELECT.listText.design}
-            onClick={handleCreateDesignCard}
-          />
-          <WrappedListItem
-            leftIcon='dev'
-            text={CAREER_SELECT.listText.dev}
-            onClick={handleCreateDevCard}
-          />
+          <WrappedListItem leftIcon="design" text={CAREER_SELECT.listText.design} onClick={handleCreateDesignCard} />
+          <WrappedListItem leftIcon="dev" text={CAREER_SELECT.listText.dev} onClick={handleCreateDevCard} />
         </List>
       </section>
     </main>
-  )
+  );
 }
 
 export default NewCreateCardView;
