a209890bf099a308fa40f649b2c3b33574abc5c8
youngju6143 <youngju6143@naver.com>
2025-03-15 15:38:53 +0900

    💄 Style: 선택된 태그 이동하는 애니메이션 구현 완성

--- src/features/multi-step-form/ui/careerForm/tagFormStep/ui/TagBox.tsx
+++ src/features/multi-step-form/ui/careerForm/tagFormStep/ui/TagBox.tsx
@@ -4,7 +4,7 @@ import React, { Dispatch, useEffect } from 'react';
 import { cn } from '@/shared/lib/utils';
 import Tag from '@/shared/ui/tag/tag';
 
-import { tagConfig } from '../config/config';
+import { tagConfig, TagConfigItem } from '../config/config';
 
 type TagBoxPropsType = {
   tagCount: number;
@@ -18,11 +18,6 @@ type TagBoxPropsType = {
  *
  */
 function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsType) {
-  useEffect(() => {
-    console.log(tagCount);
-    console.log(tagArray);
-  }, [tagCount]);
-
   function handleTagClick(tagMessage: string) {
     if (!tagArray.includes(tagMessage)) {
       setTagCount(tagCount + 1);
@@ -33,11 +28,65 @@ function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsTyp
     }
   }
 
+  // 선택된 태그가 4개 이상일 때의 고정 좌표, 추후 파일 분리
+  const tagFixedPositions: Record<string, string> = {
+    '대표 프로젝트': 'right-[calc(50%-96px)] bottom-[calc(50%-5px)]',
+    '작성한 글': 'left-[calc(50%-96px)] bottom-[calc(50%-5px)]',
+    SNS: 'right-[calc(50%)] top-[calc(50%+9px)]',
+    취미: 'right-[calc(50%-60px)] bottom-[calc(50%-50px)]',
+    '최근 소식': 'left-[calc(50%+4px)] bottom-[calc(50%+39px)]',
+    '활동 지역': 'left-[calc(50%-88px)] top-[calc(50%-80px)]',
+    '소속 정보': 'left-[calc(50%-40px)] top-[calc(50%-124px)]',
+  };
+
+  // 선택된 태그의 좌표 이동
+  function getTagPositions(position: string, tag: TagConfigItem) {
+    if (tagArray.length >= 4 && tagArray.includes(tag.message)) {
+      return tagFixedPositions[tag.message];
+    }
+
+    if (tagArray[0] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-56px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-56px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%+16px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%+16px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%+16px)]';
+    }
+    if (tagArray[1] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-104px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-104px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%+60px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%+60px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%+60px)]';
+    }
+    if (tagArray[2] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-12px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-12px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%-32px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%-32px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%-32px)]';
+    }
+  }
+
+  useEffect(() => {
+    console.log(tagCount);
+    console.log(tagArray);
+  }, [tagCount]);
+
   return (
     <>
       {tagConfig.map((tag) => {
         return (
-          <div key={tag.id} onClick={() => handleTagClick(tag.message)}>
+          <div key={tag.id}>
             <Tag
               message={tag.message}
               size="lg"
@@ -45,11 +94,9 @@ function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsTyp
                 'transition-all duration-500 ease-in-out',
                 !tagArray.includes(tag.message) && tag.animation,
                 tag.className,
-                tag.position,
-                tagArray[0] === tag.message ? 'move-to-one' : tag.position,
-                tagArray[1] === tag.message ? 'move-to-two' : tag.position,
-                tagArray[2] === tag.message ? 'move-to-three' : tag.position,
+                tagArray.includes(tag.message) ? getTagPositions(tag.position, tag) : tag.position,
               )}
+              onClick={() => handleTagClick(tag.message)}
             />
           </div>
         );
