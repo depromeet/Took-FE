82935e706aac8f0613dc99d334000679f9d478f9
semnil5202 <semnil5202@gmail.com>
2025-03-15 15:08:44 +0900

    design: add AddCard background

--- src/features/home/components/BusinessCard/AddCard.tsx
+++ src/features/home/components/BusinessCard/AddCard.tsx
@@ -2,9 +2,12 @@ import { Typography } from '@/shared/ui/typography';
 
 import { CardPlusIcon } from '../icons/CardPlusIcon';
 
+import { AddCardBackground } from './Background/AddCradBackground';
+
 export const AddCard = () => {
   return (
-    <div className="bg-[rgba(255,255,255, 0.2)] relative flex h-[394px] w-[270px] flex-col items-center justify-center gap-2 overflow-hidden rounded-2xl">
+    <div className="relative flex h-[394px] w-[270px] flex-col items-center justify-center gap-2 overflow-hidden rounded-2xl bg-[rgba(255,255,255,0.1)]">
+      <AddCardBackground className="absolute top-0" />
       <Typography variant="body-1">추가하기</Typography>
       <CardPlusIcon />
     </div>
