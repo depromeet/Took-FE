5685390808749fb321c61d2ef197b47d7215ff35
semnil5202 <semnil5202@gmail.com>
2025-03-15 17:27:20 +0900

    feat: navbar 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
+import { Navbar } from '@/shared/ui/Navigation';
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-import { CardContainer } from '@/features/home/containers/CardContainer';
-import Appbar from '@/shared/ui/appbar';
 
 export default function Home() {
   return (
     <div className="mx-auto h-dvh w-full max-w-[600px] justify-center">
-      <Appbar page="main" />
-      <CardContainer />
+      <Navbar />
     </div>
   );
 }
