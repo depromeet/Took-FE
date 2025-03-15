50c3d8fbecb9c857158d8c2d5594c08674f1fc1d
영주 <111039206+youngju6143@users.noreply.github.com>
2025-03-15 16:40:44 +0900

    💄 Style: 선택된 태그 이동하는 애니메이션 구현 완성 (#79)
    * 💄 Style: 선택된 태그 이동하는 애니메이션 구현 완성
    
    * 🐛 Fix: step 순서 변경
    
    * ♻️ Refactor: 상수 변수화 및 fixedPosition config로 이동

--- src/features/multi-step-form/ui/careerForm/tagFormStep/config/config.ts
+++ src/features/multi-step-form/ui/careerForm/tagFormStep/config/config.ts
@@ -4,9 +4,10 @@ export type SelectedTagType = '대표 프로젝트' | '작성한 글' | 'SNS' | 
 
 export type TagConfigItem = {
   id: number;
-  message: SelectedTagType; // ✅ 여기에 명확하게 타입 지정
+  message: SelectedTagType;
   className: string;
   position: string;
+  fixedPosition: string;
   title?: string;
   description?: string;
   animation: string;
@@ -18,7 +19,7 @@ export const tagConfig: TagConfigItem[] = [
     message: '대표 프로젝트',
     className: tagStyle,
     position: 'bottom-28 right-0',
-    // position: 'top-72 left-52',
+    fixedPosition: 'right-[calc(50%-96px)] bottom-[calc(50%-5px)]',
     title: '프로젝트 제목',
     description: '김디퍼님의 프로젝트 링크',
     animation: 'downandup',
@@ -28,7 +29,7 @@ export const tagConfig: TagConfigItem[] = [
     message: '작성한 글',
     className: tagStyle,
     position: 'bottom-16 left-12',
-    // position: 'top-80 left-12',
+    fixedPosition: 'left-[calc(50%-96px)] bottom-[calc(50%-5px)]',
     title: '작성한 글 제목',
     description: '김디퍼님의 게시물 링크',
     animation: 'downandup',
@@ -37,8 +38,8 @@ export const tagConfig: TagConfigItem[] = [
     id: 3,
     message: 'SNS',
     className: tagStyle,
-    // position: 'top-16 left-64',
     position: `right-0 top-16`,
+    fixedPosition: 'right-[calc(50%)] top-[calc(50%+9px)]',
     title: 'SNS 아이디',
     animation: 'upanddown',
   },
@@ -47,7 +48,7 @@ export const tagConfig: TagConfigItem[] = [
     message: '취미',
     className: tagStyle,
     position: 'bottom-8 right-20',
-    // position: 'top-[23rem] left-44',
+    fixedPosition: 'right-[calc(50%-60px)] bottom-[calc(50%-50px)]',
     description: `고소한 커피 한 잔과 함께 노트북을 켜고 새\n로운 아이디어를 정리하는 걸 좋아해요`,
     animation: 'upanddown',
   },
@@ -55,8 +56,8 @@ export const tagConfig: TagConfigItem[] = [
     id: 5,
     message: '최근 소식',
     className: tagStyle,
-    // position: 'top-64',
-    position: 'bottom-40',
+    position: 'bottom-40 left-0',
+    fixedPosition: 'left-[calc(50%+4px)] bottom-[calc(50%+39px)]',
     description: `부동산 스타트업에서 2년간 일하다가 \n 퇴사하고 지금은 이직 준비 중이에요`,
     animation: 'upanddown',
   },
@@ -64,7 +65,8 @@ export const tagConfig: TagConfigItem[] = [
     id: 6,
     message: '활동 지역',
     className: tagStyle,
-    position: `top-20`,
+    position: `top-20 left-0`,
+    fixedPosition: 'left-[calc(50%-88px)] top-[calc(50%-80px)]',
     description: `주로 서울 전역에서 활동하지만, \n특히 강남에서 자주 출몰해요`,
     animation: 'upanddown',
   },
@@ -72,8 +74,8 @@ export const tagConfig: TagConfigItem[] = [
     id: 7,
     message: '소속 정보',
     className: tagStyle,
-    // position: 'top-0 left-28',
-    position: `left-1/2 -translate-x-1/2`,
+    position: 'left-40 top-0',
+    fixedPosition: 'left-[calc(50%-40px)] top-[calc(50%-124px)]',
     animation: 'downandup',
   },
 ];
--- src/features/multi-step-form/ui/careerForm/tagFormStep/ui/TagBox.tsx
+++ src/features/multi-step-form/ui/careerForm/tagFormStep/ui/TagBox.tsx
@@ -1,10 +1,10 @@
 'use client';
-import React, { Dispatch, useEffect } from 'react';
+import React, { Dispatch } from 'react';
 
 import { cn } from '@/shared/lib/utils';
 import Tag from '@/shared/ui/tag/tag';
 
-import { tagConfig } from '../config/config';
+import { tagConfig, TagConfigItem } from '../config/config';
 
 type TagBoxPropsType = {
   tagCount: number;
@@ -13,16 +13,13 @@ type TagBoxPropsType = {
   setTagArray: Dispatch<React.SetStateAction<string[]>>;
 };
 
+const MAX_DYNAMIC_TAGS = 4;
+
 /**
  * TagBox : 태그 박스
  *
  */
 function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsType) {
-  useEffect(() => {
-    console.log(tagCount);
-    console.log(tagArray);
-  }, [tagCount]);
-
   function handleTagClick(tagMessage: string) {
     if (!tagArray.includes(tagMessage)) {
       setTagCount(tagCount + 1);
@@ -33,11 +30,49 @@ function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsTyp
     }
   }
 
+  // 선택된 태그의 좌표 이동
+  function getTagPositions(position: string, tag: TagConfigItem) {
+    if (tagArray.length >= MAX_DYNAMIC_TAGS && tagArray.includes(tag.message)) {
+      return tag.fixedPosition;
+    }
+
+    if (tagArray[0] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-56px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-56px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%+16px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%+16px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%+16px)]';
+    }
+    if (tagArray[1] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-104px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-104px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%+60px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%+60px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%+60px)]';
+    }
+    if (tagArray[2] === tag.message) {
+      if (position.includes('top') && position.includes('left')) return 'left-[calc(50%-40px)] top-[calc(50%-12px)]';
+      if (position.includes('top') && position.includes('right')) return 'right-[calc(50%-28px)] top-[calc(50%-12px)]';
+      if (position.includes('bottom') && position.includes('right')) {
+        if (tag.message === '취미') return 'right-[calc(50%-28px)] bottom-[calc(50%-32px)]';
+        if (tag.message === '대표 프로젝트') return 'right-[calc(50%-52px)] bottom-[calc(50%-32px)]';
+      }
+      if (position.includes('bottom') && position.includes('left'))
+        return 'left-[calc(50%-40px)] bottom-[calc(50%-32px)]';
+    }
+  }
+
   return (
     <>
       {tagConfig.map((tag) => {
         return (
-          <div key={tag.id} onClick={() => handleTagClick(tag.message)}>
+          <div key={tag.id}>
             <Tag
               message={tag.message}
               size="lg"
@@ -45,11 +80,9 @@ function TagBox({ tagCount, tagArray, setTagCount, setTagArray }: TagBoxPropsTyp
                 'transition-all duration-500 ease-in-out',
                 !tagArray.includes(tag.message) && tag.animation,
                 tag.className,
-                tag.position,
-                tagArray[0] === tag.message ? 'move-to-one' : tag.position,
-                tagArray[1] === tag.message ? 'move-to-two' : tag.position,
-                tagArray[2] === tag.message ? 'move-to-three' : tag.position,
+                tagArray.includes(tag.message) ? getTagPositions(tag.position, tag) : tag.position,
               )}
+              onClick={() => handleTagClick(tag.message)}
             />
           </div>
         );
