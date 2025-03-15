08c9495435d2066695b7577cc994d40ebcbb59c8
jangwonyoon <wkddnjs7310@naver.com>
2025-03-15 16:02:59 +0900

    ✨ Feat: 공통 컴포넌트 - 타이포그라피 color 지원

--- src/shared/ui/typography/typography.ts
+++ src/shared/ui/typography/typography.ts
@@ -14,6 +14,22 @@ export const typography = cva('typography', {
       'caption-1': 'text-caption-1',
       'caption-2': 'text-caption-2',
     },
+    color: {
+      'gray-100': 'text-gray-100',
+      'gray-200': 'text-gray-200',
+      'gray-300': 'text-gray-300',
+      'gray-400': 'text-gray-400',
+      'gray-500': 'text-gray-500',
+      'gray-600': 'text-gray-600',
+      'gray-700': 'text-gray-700',
+      'gray-800': 'text-gray-800',
+      'gray-900': 'text-gray-900',
+      'gray-white': 'text-gray-white',
+      'error-light': 'text-error-light',
+      'error-medium': 'text-error-medium',
+      'error-normal': 'text-error-normal',
+      'error-dark': 'text-error-dark',
+    },
   },
   // default 글꼴로 설정 body-1
   defaultVariants: {
