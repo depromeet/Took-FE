83722fd202aa5ff74e40f8fc0c5cf206650f3a4b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-15 15:24:49 +0900

    ♻️refactor :  명함 상세 페이지 배경 색 수정

--- src/app/(detail)/card-detail/[id]/page.tsx
+++ src/app/(detail)/card-detail/[id]/page.tsx
@@ -4,7 +4,7 @@ import CardTabs from '@/features/card-detail/ui/cardTabs';
 function Page() {
   return (
     <div className="flex h-dvh w-full">
-      <div className="mx-auto flex w-full max-w-[600px] flex-col items-center">
+      <div className="mx-auto flex w-full max-w-[600px] flex-col items-center bg-gray-black">
         <CardDetailHeader />
         <CardTabs />
       </div>
