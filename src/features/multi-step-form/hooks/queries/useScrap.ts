aff26dc755f79b2cc4fc7a0632f5a00804f8a36b
jangwonyoon <wkddnjs7310@naver.com>
2025-03-17 01:10:46 +0900

    ✨ Feat: 스크래핑 api 추가

--- src/features/multi-step-form/hooks/queries/useScrap.ts
+++ src/features/multi-step-form/hooks/queries/useScrap.ts
@@ -1,32 +1,40 @@
 import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
 import { client } from '@/shared/apis/client';
 import { CLIENT_SIDE_URL } from '@/shared/constants';
 import { ApiResponseType } from '@/shared/types';
 
-type ScrapType = 'blog' | 'project';
+type ScrapType = 'BLOG' | 'PROJECT';
 
 type ScrapPayload = {
   link: string;
 };
 
+type ScrapResponse = {
+  title: string;
+  link: string;
+  imageUrl: string;
+  description: string;
+};
+
 const postScapLink = async ({ payload, type }: { payload: ScrapPayload; type: ScrapType }) => {
-  const res = await client.post<ApiResponseType<any>>(`${CLIENT_SIDE_URL}/api/card`, payload, {
+  const res = (await client.post<ApiResponseType<ScrapResponse>>(`${CLIENT_SIDE_URL}/api/card/scrap`, payload, {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
     params: {
       type,
     },
-  });
-  return res;
+  })) as AxiosResponse<ApiResponseType<ScrapResponse>>;
+
+  return res.data;
 };
 
 export const useScrap = () => {
-  return useMutation({
+  return useMutation<ApiResponseType<ScrapResponse>, unknown, { payload: ScrapPayload; type: ScrapType }>({
     mutationFn: postScapLink,
-    onSuccess: (data) => {
-      console.log('스크랩 성공', data);
-    },
-    onError: (error) => {
-      console.log('스크랩 실패', error);
-    },
+    onSuccess: () => {},
+    onError: () => {},
   });
 };
