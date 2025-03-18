04c9994419c47fcce7257a3b3beaf685160572ba
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-18 19:20:58 +0900

    ♻️ refactor : 불필요한 fragment 제거

--- src/features/card-detail/ui/cardDetailHeader.tsx
+++ src/features/card-detail/ui/cardDetailHeader.tsx
@@ -19,59 +19,55 @@ const CardDetailHeader = () => {
   const currentJob = JOB_CONFIG[userJob];
 
   return (
-    <>
-      <div
-        className="card-detail-header w-full bg-cover bg-center pb-[40px]"
-        style={{
-          backgroundImage: `url('${currentJob.backgroundImage}')`,
-        }}
-      >
-        {/* 카드 상세 헤더 */}
-        <Appbar page="detail" />
-        {/* 카드 상세 userData */}
-        <div className={`flex w-full items-start ${spacingStyles({ marginTop: 'ms' })}`}>
-          <div className={`flex w-full flex-col items-start ${spacingStyles({ paddingX: 'md' })}`}>
-            <div className="flex w-full items-center justify-between">
-              {/* 프로필 이미지 */}
-              <div
-                className={`flex h-[56px] w-[56px] items-center justify-center rounded-full bg-gray-100 ${spacingStyles({ marginBottom: 'ms' })}`}
-              >
-                <Image src="/icons/avatarIcon.svg" alt="Settings" width="28" height="28" className="rounded-full" />
-              </div>
-              {/* 개발자 , 디자이너 아이콘 */}
-              <Image src={currentJob.iconPath} alt={currentJob.iconAlt} width="30" height="30" />
-            </div>
-            <p className="title-1 line-clamp-1">{data.data.nickname}</p>
+    <div
+      className="card-detail-header w-full bg-cover bg-center pb-[40px]"
+      style={{
+        backgroundImage: `url('${currentJob.backgroundImage}')`,
+      }}
+    >
+      {/* 카드 상세 헤더 */}
+      <Appbar page="detail" />
+      {/* 카드 상세 userData */}
+      <div className={`flex w-full items-start ${spacingStyles({ marginTop: 'ms' })}`}>
+        <div className={`flex w-full flex-col items-start ${spacingStyles({ paddingX: 'md' })}`}>
+          <div className="flex w-full items-center justify-between">
+            {/* 프로필 이미지 */}
             <div
-              className={`${spacingStyles({ marginBottom: 'ms' })} line-clamp-1 flex w-full items-center text-title-3`}
+              className={`flex h-[56px] w-[56px] items-center justify-center rounded-full bg-gray-100 ${spacingStyles({ marginBottom: 'ms' })}`}
             >
-              <span className={`max-w-1/2 truncate ${spacingStyles({ marginRight: 'sm' })}`}>
-                {data?.data.detailJob}
-              </span>
-              {data?.data && (
-                <>
-                  <span className="flex-shrink-0 px-1">|</span>
-                  <span className={`max-w-1/2 truncate ${spacingStyles({ marginLeft: 'sm' })}`}>
-                    {data?.data.organization}
-                  </span>
-                </>
-              )}
+              <Image src="/icons/avatarIcon.svg" alt="Settings" width="28" height="28" className="rounded-full" />
             </div>
-            <p className="body-5 line-clamp-2">{data?.data.summary}</p>
-            <div className={`${spacingStyles({ marginTop: 'md' })} flex items-center`}>
-              <Image
-                src="/icons/region.svg"
-                alt="지역"
-                width={16}
-                height={16}
-                className={`${spacingStyles({ marginRight: 'xs' })}`}
-              />
-              <p className="line-clamp-1 text-body-5">{data?.data.region}</p>
-            </div>
+            {/* 개발자 , 디자이너 아이콘 */}
+            <Image src={currentJob.iconPath} alt={currentJob.iconAlt} width="30" height="30" />
+          </div>
+          <p className="title-1 line-clamp-1">{data.data.nickname}</p>
+          <div
+            className={`${spacingStyles({ marginBottom: 'ms' })} line-clamp-1 flex w-full items-center text-title-3`}
+          >
+            <span className={`max-w-1/2 truncate ${spacingStyles({ marginRight: 'sm' })}`}>{data?.data.detailJob}</span>
+            {data?.data && (
+              <>
+                <span className="flex-shrink-0 px-1">|</span>
+                <span className={`max-w-1/2 truncate ${spacingStyles({ marginLeft: 'sm' })}`}>
+                  {data?.data.organization}
+                </span>
+              </>
+            )}
+          </div>
+          <p className="body-5 line-clamp-2">{data?.data.summary}</p>
+          <div className={`${spacingStyles({ marginTop: 'md' })} flex items-center`}>
+            <Image
+              src="/icons/region.svg"
+              alt="지역"
+              width={16}
+              height={16}
+              className={`${spacingStyles({ marginRight: 'xs' })}`}
+            />
+            <p className="line-clamp-1 text-body-5">{data?.data.region}</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
