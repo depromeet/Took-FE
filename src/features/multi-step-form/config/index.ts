2a1b1d753aa66bee11e66a2197f9bda8df7d4957
jangwonyoon <wkddnjs7310@naver.com>
2025-03-18 15:06:31 +0900

    🔥 Refactor: 사용하지 않는 코드 제거

--- src/features/multi-step-form/config/index.ts
+++ src/features/multi-step-form/config/index.ts
@@ -21,55 +21,6 @@ export const TOTAL_STEPS = 4;
 export const MINIMUM_STEP = 1;
 export const MAXIMUM_ADD = 10;
 
-// 테스트
-
-// 디자인 직군 옵션
-const designOptions = [
-  { value: 'designer', label: 'Designer - 디자이너', keywords: ['디자이너'] },
-  { value: 'product-designer', label: 'Product Designer - 프로덕트 디자이너', keywords: ['프로덕트 디자이너'] },
-  { value: 'graphic-designer', label: 'Graphic Designer - 그래픽 디자이너', keywords: ['그래픽 디자이너'] },
-  {
-    value: 'interaction-designer',
-    label: 'Interaction Designer - 인터랙션/인터렉션 디자이너',
-    keywords: ['인터랙션 디자이너', '인터렉션 디자이너'],
-  },
-  {
-    value: 'ux-ui-designer',
-    label: 'UX/UI Designer - 유엑스유아이 디자이너, 유엑유아 디자이너',
-    keywords: ['유엑스유아이 디자이너', '유엑유아 디자이너'],
-  },
-  {
-    value: 'ux-designer',
-    label: 'UX Designer - 유엑스 디자이너, 유엑 디자이너',
-    keywords: ['유엑스 디자이너', '유엑 디자이너'],
-  },
-  { value: 'ui-designer', label: 'UI Designer - 유아이 디자이너', keywords: ['유아이 디자이너'] },
-];
-
-// 개발 직군 옵션
-const developerOptions = [
-  { value: 'developer', label: 'Developer - 디벨로퍼, 개발자', keywords: ['디벨로퍼', '개발자'] },
-  { value: 'server-developer', label: 'Server Developer - 서버 ~', keywords: ['서버'] },
-  { value: 'ai-developer', label: 'AI Developer - 에이아이~, 에아~', keywords: ['에이아이', '에아'] },
-  { value: 'frontend-developer', label: 'Frontend Developer - 프론트~, 프론티드', keywords: ['프론트', '프론티드'] },
-  {
-    value: 'ios-developer',
-    label: 'iOS Developer - 아오스, ios, 아이오에스',
-    keywords: ['아오스', 'ios', '아이오에스'],
-  },
-  { value: 'android-developer', label: 'Android Developer', keywords: ['안드로이드'] },
-  { value: 'fullstack-developer', label: 'Full-Stack Developer - 풀스택, 풀스텍', keywords: ['풀스택', '풀스텍'] },
-  { value: 'devops-developer', label: 'DevOps Developer - 데브옵스', keywords: ['데브옵스'] },
-  { value: 'desktop-developer', label: 'Desktop Developer - 데스크톱, 데스크탑', keywords: ['데스크톱', '데스크탑'] },
-  { value: 'game-developer', label: 'Game Developer - 게임', keywords: ['게임'] },
-  {
-    value: 'data-engineering-developer',
-    label: 'Data Engineering Developer - 데이타, 다타, 데이터, 엔지니어링',
-    keywords: ['데이타', '다타', '데이터', '엔지니어링'],
-  },
-  { value: 'qa-developer', label: 'QA Developer - 큐에이, QA', keywords: ['큐에이', 'QA'] },
-];
-
 export const FIELD_TAG_MAPPING: Record<keyof Pick<CareerFormData, TagValue>, TagValue> = {
   organization: 'organization',
   sns: 'sns',
@@ -79,5 +30,3 @@ export const FIELD_TAG_MAPPING: Record<keyof Pick<CareerFormData, TagValue>, Tag
   content: 'content',
   project: 'project',
 };
-
-export const careerOptions = [...designOptions, ...developerOptions];
