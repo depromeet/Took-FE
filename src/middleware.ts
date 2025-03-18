127e110aaadd5d722da175af34ddfb275ea0f196
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-18 19:50:10 +0900

    ♻️ refactor :  미들웨어 macher 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,3 +38,16 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * 다음으로 시작하는 경로를 제외한 모든 요청 경로를 매칭합니다:
+     * - api (API 라우트)
+     * - _next/static (정적 파일)
+     * - _next/image (이미지 최적화 파일)
+     * - favicon.ico (파비콘 파일)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
