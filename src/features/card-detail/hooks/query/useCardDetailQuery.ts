a099c8276f7ca1862949e4e27fce77a08fa1e676
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-20 23:53:49 +0900

    ✨feat : 명함 상세 응답 데이터 임시 변경

--- src/features/card-detail/hooks/query/useCardDetailQuery.ts
+++ src/features/card-detail/hooks/query/useCardDetailQuery.ts
@@ -1,15 +1,23 @@
 // api/cardQueries.ts
 import { useQuery } from '@tanstack/react-query';
 
+import { MyCardDto } from '@/features/home/types';
 import { client } from '@/shared/apis/client';
 import { CLIENT_SIDE_URL } from '@/shared/constants';
 
-import { mockCardDetailData } from '../../mocks/sample';
-import { CardDetailDto } from '../../types/cardDetail';
+import { mockReceivedCardDetailData } from '../../mocks/receivedCardDetail';
+//import { CardDetailDto } from '../../types/cardDetail';
+import { ReceivedCardDetailDto } from '../../types/receivedCardDetail';
 
 // API 호출 함수
-const getCardDetail = async (cardId: number): Promise<CardDetailDto> => {
-  const data = await client.get<CardDetailDto>(`${CLIENT_SIDE_URL}/api/card/detail?cardId=${cardId}`);
+const getCardDetail = async (cardId: number): Promise<ReceivedCardDetailDto> => {
+  const data = await client.get<ReceivedCardDetailDto>(`${CLIENT_SIDE_URL}/api/card/detail?cardId=${cardId}`);
+  return data;
+};
+
+// 내 명함 목록 조회
+const getMyCard = async (): Promise<MyCardDto> => {
+  const data = await client.get<MyCardDto>(`${CLIENT_SIDE_URL}/api/card/my`);
   return data;
 };
 
@@ -20,5 +28,15 @@ export const useCardDetailQuery = (cardId: number) => {
     queryFn: () => getCardDetail(cardId),
   });
 
-  return { data: mockCardDetailData };
+  return { data: mockReceivedCardDetailData };
+};
+
+// 내 명함 목록 조회
+export const useMyCardQuery = () => {
+  const data = useQuery({
+    queryKey: ['myCard'],
+    queryFn: () => getMyCard(),
+  });
+
+  return data;
 };
