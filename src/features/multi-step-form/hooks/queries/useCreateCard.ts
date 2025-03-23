6040840a9697b4f3b26b7d8f9de1415ff93c922d
jangwonyoon <wkddnjs7310@naver.com>
2025-03-23 18:25:26 +0900

    ✨ Feat: 카드 생성 routing 적용

--- src/features/multi-step-form/hooks/queries/useCreateCard.ts
+++ src/features/multi-step-form/hooks/queries/useCreateCard.ts
@@ -4,6 +4,9 @@ import { client } from '@/shared/apis/client';
 import { CLIENT_SIDE_URL } from '@/shared/constants';
 import { ApiResponseType } from '@/shared/types';
 
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
 const createCard = async (payload: FormData) => {
   const res = await client.post<ApiResponseType<null>>(`${CLIENT_SIDE_URL}/api/card`, payload);
   return res;
@@ -11,12 +14,16 @@ const createCard = async (payload: FormData) => {
 
 // 카드 생성 API 호출
 export const useCreateCard = () => {
+  const router = useRouter();
+
   return useMutation({
     mutationFn: createCard,
-    onSuccess: (data) => {
-      console.log('카드 생성 성공', data);
+    onSuccess: () => {
+      toast.success('명함 생성 성공');
+      router.push('/home');
     },
     onError: (error) => {
+      toast.error('명함 생성 실패');
       console.error('카드 생성 실패', error);
     },
   });
