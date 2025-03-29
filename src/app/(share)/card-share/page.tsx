0e4bbf1469196ee3b75792b12146982567e84de3
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-29 15:38:07 +0900

    ✨feat : card-share 페이지 생성

---
+++ src/app/(share)/card-share/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AnimatedShareCardWrapper from '@/features/share/container/AnimatedShareCardWrapper';
+
+function Page() {
+  return (
+    <div className="relative mx-auto h-dvh w-full max-w-[600px] justify-center">
+      <AnimatedShareCardWrapper />
+    </div>
+  );
+}
+
+export default Page;
