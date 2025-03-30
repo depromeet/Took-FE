7da2c43c164a922a4f6651e9461f02f062f726e4
semnil5202 <semnil5202@gmail.com>
2025-03-30 20:23:20 +0900

    wip

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from 'sonner';
 
 import { QrContainer } from '@/features/share/containers/QrContainer';
+import { SegmentContainer } from '@/features/share/containers/SegmentContainer';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -8,6 +9,7 @@ export default function Share() {
   return (
     <>
       <div className="relative mx-auto h-dvh w-full max-w-[600px] overflow-x-hidden">
+        <SegmentContainer className="absolute" />
         <QrContainer />
       </div>
       <Toaster position="top-center" />
---
+++ src/features/share/containers/SegmentContainer.tsx
@@ -0,0 +1,11 @@
+import { HTMLAttributes } from 'react';
+
+import { cn } from '@/shared/lib/utils';
+
+export const SegmentContainer = ({ className, ...restProps }: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div className={cn(className)} {...restProps}>
+      dseg
+    </div>
+  );
+};
