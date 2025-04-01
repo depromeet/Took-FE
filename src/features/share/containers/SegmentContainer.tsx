4c20c2f1f8e5c9c38fb5a25e47bce35fa45ee266
semnil5202 <semnil5202@gmail.com>
2025-04-02 00:24:36 +0900

    refactor: 불필요한 코드 제거 및 todo 주석 생성

--- src/features/share/containers/SegmentContainer.tsx
+++ src/features/share/containers/SegmentContainer.tsx
@@ -3,9 +3,5 @@ import { HTMLAttributes } from 'react';
 import { cn } from '@/shared/lib/utils';
 
 export const SegmentContainer = ({ className, ...restProps }: HTMLAttributes<HTMLDivElement>) => {
-  return (
-    <div className={cn(className)} {...restProps}>
-      dseg
-    </div>
-  );
+  return <div className={cn(className)} {...restProps}></div>;
 };
