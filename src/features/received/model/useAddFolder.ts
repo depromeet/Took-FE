beba2c34ff0e2d4b1c5c83045b6ca64755457c34
youngju6143 <youngju6143@naver.com>
2025-04-10 14:41:37 +0900

    ♻️ Refactor: useAddFolder, useUpdateFolder 훅 분리 및 엔터키 중복 입력 시 폴더 추가/수정이 여러 번 되는 이슈 방지

---
+++ src/features/received/model/useAddFolder.ts
@@ -0,0 +1,51 @@
+import { MutableRefObject, useState } from 'react';
+
+import { useCreateFolder } from './mutations/useCreateFolder';
+import { useFolderStore } from './store/useFoldersStore';
+
+type UseAddFolderProps = {
+  isSubmittingRef: MutableRefObject<boolean>;
+  closeModal: () => void;
+};
+const MAX_LENGTH = 10;
+
+export const useAddFolder = ({ isSubmittingRef, closeModal }: UseAddFolderProps) => {
+  const [isAdd, setIsAdd] = useState<boolean>(false); // 추가하기 버튼 누름 여부
+  const [newFolderName, setNewFolderName] = useState<string>(''); // 수정하려는 폴더의 새로운 이름
+
+  const { addFolder } = useFolderStore();
+  const { mutate: serverCreateFolder } = useCreateFolder(); // createFolder 함수와 로딩 상태
+
+  const handleAdd = () => {
+    setIsAdd(true);
+  };
+  const handleAddChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewFolderName(e.target.value);
+  };
+  const handleAddKeyDown = (newFolderName: string, e?: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e?.nativeEvent.isComposing) return;
+    if (e?.key !== 'Enter') return;
+    if (isSubmittingRef.current) return;
+
+    e.preventDefault();
+    isSubmittingRef.current = true;
+
+    if (newFolderName.length <= MAX_LENGTH) {
+      serverCreateFolder(newFolderName);
+      addFolder(newFolderName);
+      closeModal();
+    }
+
+    setTimeout(() => {
+      isSubmittingRef.current = false;
+    }, 500);
+  };
+  return {
+    isAdd,
+    setIsAdd,
+    newFolderName,
+    handleAdd,
+    handleAddChange,
+    handleAddKeyDown,
+  };
+};
