80c4bbdf081de198bd9b31752e4d353fe5f7e542
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-04-14 10:14:17 +0900

    ✨ feat: 한  줄 메모 api 훅 분리

--- src/features/card-notes/hooks/queries/useCardQuery.ts
+++ src/features/card-notes/hooks/queries/useCardQuery.ts
@@ -2,19 +2,22 @@ import { useQuery } from '@tanstack/react-query';
 
 import { client } from '@/shared/apis/client';
 
-import { MyCardDto } from '../../types/index';
+import { MOCK_DATA } from '../../mocks';
+import { CardNotesDto } from '../../types/index';
 
 export const MY_CARD_NOTES_QUERY_KEY = 'MY_CARD_QUERY_KEY';
 
 const _getCard = async () => {
-  const { data } = await client.get<MyCardDto>(`/api/card/my`);
+  const { data } = await client.get<CardNotesDto>(`/api/card/my`);
 
   return data;
 };
 
 export const useCardQuery = () => {
-  return useQuery({
+  const { data: _ } = useQuery({
     queryKey: [MY_CARD_NOTES_QUERY_KEY],
     queryFn: () => _getCard(),
   });
+
+  return { data: MOCK_DATA };
 };
