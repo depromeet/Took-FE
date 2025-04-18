803371762923c818f0bd0ed209b6b493637ae8fe
youngju6143 <youngju6143@naver.com>
2025-04-18 23:57:34 +0900

    🚑️ Hotfix: 페이지 이동 시 로딩 추가, 알림 페이지 라우팅 추가

--- src/features/home/containers/HeaderContainer.tsx
+++ src/features/home/containers/HeaderContainer.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import Appbar from '@/shared/ui/appbar';
+import { useRouter } from 'next/navigation';
 
 export const HeaderContainer = () => {
+  const router = useRouter();
+
+  const goToAlarmPage = () => {
+    router.push('/setting/alram');
+  };
+
   return (
     <section>
-      <Appbar page="main" />
+      <Appbar page="main" onRightClick={goToAlarmPage} />
     </section>
   );
 };
